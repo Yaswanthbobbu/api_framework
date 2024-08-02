44daab7fd2ddfd62ee71ac9189dfe369bf97170c
munibabuc <30755405+munibabuc@users.noreply.github.com>
2024-08-02 16:52:23 +0530

    Update test_sample.py

--- tests/authentication/test_sample.py
+++ tests/authentication/test_sample.py
@@ -7,7 +7,6 @@ from library.utils import *
 from library.utils import setup_logger
 logger = setup_logger(__name__)
 
-
 def test_login():
     logger.info("User tries to Login: %s", BASE_URL + CREATE_USER_ENDPOINT)
     access_token = get_access_token()
@@ -24,19 +23,21 @@ def create_user_payload():
         payload = json.load(file)
     return payload
 
-
 def test_create_user(create_user_payload):
-    access_token = get_access_token()
-    currUser = access_token.split(';')[4]
-    headers = {"accesstoken": f"{access_token}", "currUser": f"{currUser}", "Content-Type": "application/json"}
-    response = make_request("POST", CREATE_USER_ENDPOINT, headers=headers, payload=create_user_payload, verify=False)
-    logger.info("Request Headers: %s", headers)
-    logger.info("Request Payload: %s", create_user_payload)
-    logger.info("Response Status Code: %s", response.status_code)
-    logger.info("Response Content: %s", response.content)
-    # logger.info("Response Content: %s", response.json())
-    if response.status_code == 201:
-        logger.info("Creation of new user is Successful")
-    else:
-        logger.error("Failed to create new user")
-
+    try:
+        access_token = get_access_token()
+        currUser = access_token.split(';')[4]
+        newaccess_token = access_token.split(';')[0]
+        headers = {"accessToken": f"{newaccess_token}", "currUser": f"{currUser}", "Content-Type": "application/json"}
+        response = make_request("POST", CREATE_USER_ENDPOINT, headers=headers, payload=create_user_payload, verify=False)
+        logger.info("Request Headers: %s", headers)
+        logger.info("Request Payload: %s", create_user_payload)
+        logger.info("Response Status Code: %s", response.status_code)
+        logger.info("Response Content: %s", response.content)
+        # logger.info("Response Content: %s", response.json())
+        if response.status_code == 201:
+            logger.info("Creation of new user is Successful")
+        else:
+            logger.error("Failed to create new user")
+    except Exception as error:
+        raise f"api error: {error}"
